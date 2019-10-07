5b8dd5bf5baca66f312394672cdf1abcf02da632
Chinenyem Nwadiugwu <chnw1947@gmail.com>
2019-10-07 00:00:30 -0400

    files for app

---
+++ config/keys.js
@@ -0,0 +1,13 @@
+module.exports = {
+	mongoURI:'mongodb://<username>:<password>@chinenyemcluster-shard-00-00-wj6l7.mongodb.net:27017,chinenyemcluster-shard-00-01-wj6l7.mongodb.net:27017,chinenyemcluster-shard-00-02-wj6l7.mongodb.net:27017/chinenyemcluster-shard-0?ssl=true&replicaSet=ChinenyemCluster-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: "secret",
+}
+
+// {
+//     "_id": "5d99271d9412bf605b1ae25c",
+//     "name": "Chinenyem",
+//     "email": "chnw1947@gmail.com",
+//     "password": "$2a$10$DV/X4ycdNocejXOLkNFJ3eezKvqq08BVBMR8uWo36/xsJMoD9osny",
+//     "date": "2019-10-05T23:28:29.113Z",
+//     "__v": 0
+// }
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+// DB Config
+const db = require("./config/keys").mongoURI,
+      rep = require("./config/keys").replicaSet;
+// Connect to MongoDB
+
+// const uri = "mongodb+srv://shahid:<PASSWORD>@cluster0-1q7ty.mongodb.net/test"
+// MongoClient.connect(uri, function(err, client) {
+//    if(err) {
+//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//    }
+//    console.log('Connected...');
+//    const collection = client.db("test").collection("devices");
+//    // perform actions on the collection object
+//    client.close();
+// });
+mongoose
+  .connect(
+    db,
+    {
+      // reconnectTries: 100,
+      // reconnectInterval: 500,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      //dbName: 'test'
+   }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log('heelo ' + err) );
+
+  // Passport middleware
+  app.use(passport.initialize());
+  // Passport config
+  require("./config/passport")(passport);
+  // Routes
+  app.use("/api/users", users);
+
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+app.listen(() => console.log(process.env.PORT))
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
